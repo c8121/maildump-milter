cfb22dabb1880172c55bca86948b193275713f1f
Christian Hau <ch.hau@gefro.de>
2021-09-24 16:07:06 +0200

    Cleanup: Place net & smtp function into separate files

--- src/mailforward.c
+++ src/mailforward.c
@@ -29,6 +29,9 @@
 #include <netdb.h> 
 #include <sysexits.h>
 
+#include "net_util.c"
+#include "smtp_socket_util.c"
+
 char *smtpHost;
 int smtpPort;
 char *envFrom;
@@ -66,115 +69,6 @@ void message_line_free(message_line_t *start) {
     free(start->line);
     free(start->next);
 }
-
-/**
- * 
- */
-int resolve(const char *serverName, char *resolvedName, int size) {
-    
-    struct addrinfo hints;
-    memset (&hints, 0, sizeof (hints));
-    hints.ai_family = PF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags |= AI_CANONNAME;
-    
-    
-    struct addrinfo *info;
-    int r = getaddrinfo(serverName, NULL, &hints, &info);
-    if( r != 0 ) {
-        fprintf(stderr, "Cannot resolve host: %s\n", serverName);
-        return r;
-    }
-    
-    struct addrinfo *result = info;
-    while( result ) {
-    
-        void *ptr;
-        if( result->ai_family == AF_INET ) {
-            ptr = &((struct sockaddr_in *) result->ai_addr)->sin_addr;
-        } else if( result->ai_family == AF_INET6 ) {
-            ptr = &((struct sockaddr_in6 *) result->ai_addr)->sin6_addr;
-        }
-        
-        inet_ntop( result->ai_family, ptr, resolvedName, size );
-        
-        result = result->ai_next;
-    }
-    
-    return 0;
-}
-
-/**
- * 
- */
-int open_socket(const char *serverName, int port) {
-    
-    char ip[255];
-    resolve(serverName, ip, sizeof(ip));
-    if( ip[0] == '\0' ) {
-        return -1;
-    }
-    
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if( sockfd < 0 ) {
-        fprintf(stderr, "Failed to create socket\n");
-        return -1;
-    }
-    
-    struct sockaddr_in serv_addr;
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(port);
-    
-    if(inet_pton(AF_INET, ip, &serv_addr.sin_addr)<=0) {
-        fprintf(stderr, "Invalid address/ Address not supported: %s\n", serverName);
-        close(sockfd);
-        return -1;
-    }
-    
-    if (connect(sockfd,(struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-        fprintf(stderr, "Connection failed: %s:%i\n", serverName, port);
-        close(sockfd);
-        sockfd = -1;
-    }
-    
-    return sockfd;
-}
-
-/**
- * Read and check server response.
- * Return -1 on error response, 0 otherwise
- */
-int read_line(int socket) {
-
-    char response[255];
-    char c;
-    int i=0;
-    while( 1 ) {
-        
-        int n = read(socket, &c, 1);
-        if( n < 1 ) {
-            return -1;
-        }
-        
-        if( c == '\n' ) {
-            
-            if( i > 0 && (response[0] == '2' || response[0] == '3') ) {
-                return 0;
-            } else {
-                fprintf(stderr, "Got error response: %s\n", response);
-                return -1;
-            }
-        }
-        
-        if( i < 254 ) {
-            response[i] = c;
-            response[i+1] ='\0';
-            i++;
-        }
-    }
-    
-}
-
 
 /**
  * 
@@ -250,31 +144,31 @@ int send_file(const char *filePath) {
     if( socket > 0 ) {
         
         //Read SMTP Greeting
-        if( read_line(socket) != 0 ) {
+        if( smtp_read(socket) != 0 ) {
             return -1;
         }
         
         snprintf(buf, sizeof(buf), "helo %s\r\n", heloName);
         write(socket, buf, strlen(buf));
-        if( read_line(socket) != 0 ) {
+        if( smtp_read(socket) != 0 ) {
             return -1;
         }
         
         snprintf(buf, sizeof(buf), "mail from: <%s>\r\n", envFrom);
         write(socket, buf, strlen(buf));
-        if( read_line(socket) != 0 ) {
+        if( smtp_read(socket) != 0 ) {
             return -1;
         }
         
         snprintf(buf, sizeof(buf), "rcpt to: <%s>\r\n", envTo);
         write(socket, buf, strlen(buf));
-        if( read_line(socket) != 0 ) {
+        if( smtp_read(socket) != 0 ) {
             return -1;
         }
         
         snprintf(buf, sizeof(buf), "data\r\n");
         write(socket, buf, strlen(buf));
-        if( read_line(socket) != 0 ) {
+        if( smtp_read(socket) != 0 ) {
             return -1;
         }
         
@@ -303,13 +197,13 @@ int send_file(const char *filePath) {
         
         snprintf(buf, sizeof(buf), ".\r\n");
         write(socket, buf, strlen(buf));
-        if( read_line(socket) != 0 ) {
+        if( smtp_read(socket) != 0 ) {
             return -1;
         }
         
         snprintf(buf, sizeof(buf), "quit\r\n");
         write(socket, buf, strlen(buf));
-        if( read_line(socket) != 0 ) {
+        if( smtp_read(socket) != 0 ) {
             return -1;
         }
         
