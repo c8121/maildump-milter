62845fb230aadb125b682cf6c814e8487f2d15a8
Christian Hau <ch.hau@gefro.de>
2023-02-14 20:38:36 +0100

    Fix: Check for null-char; determine last char faster

--- src/lib/base64.c
+++ src/lib/base64.c
@@ -29,7 +29,7 @@ struct base64_encoding_buffer {
 struct base64_decoding_buffer {
 	unsigned char *s;
 	size_t len;
-	unsigned char *encoded; //Data which has not been encoded so far (input length was not devidable by 4 or not padded) 
+	unsigned char *encoded; //Data which has not been encoded so far (input length was not dividable by 4 or not padded)
 	size_t encoded_len;
 	size_t encoded_avail;
 };
@@ -51,10 +51,10 @@ struct base64_encoding_buffer* base64_create_encoding_buffer(int max_line_length
  * 
  */
 void base64_free_encoding_buffer(struct base64_encoding_buffer *buf) {
-	
+
 	if( buf->s != NULL )
 		free(buf->s);
-	
+
 	free(buf);
 }
 
@@ -79,11 +79,13 @@ size_t base64_encoded_size(size_t len) {
 void base64_encode_chunk(struct base64_encoding_buffer *buf, unsigned char *s, size_t len) {
 
 	if( buf->s != NULL ) {
-		size_t last = buf->encoded_len;
-		while( buf->s[last] == '\r' || buf->s[last] == '\n' )
-			last--;
-		if( buf->s[last] == '=' ) {
-			fprintf(stderr, "Cannot append chunk, buffer was padded (please take care to choose a length devidable by 3 when appending chunks; only last chunk can have different size)\n");
+
+        unsigned char *p = buf->s + buf->encoded_len;
+        while( *p == '\r' || *p == '\n' || *p == '\0')
+            p--;
+
+		if( *p == '=' ) {
+			fprintf(stderr, "Cannot append chunk, buffer was padded (please take care to choose a length dividable by 3 when appending chunks; only last chunk can have different size)\n");
 			exit(EX_IOERR);
 		}
 	}
@@ -163,15 +165,15 @@ void base64_encode_chunk(struct base64_encoding_buffer *buf, unsigned char *s, s
  */
 unsigned int __base64_val(unsigned char c)
 {
-	if ('A' <= c && c <= 'Z') 
+	if ('A' <= c && c <= 'Z')
 		return c - 'A';
-	if ('a' <= c && c <= 'z') 
+	if ('a' <= c && c <= 'z')
 		return 26 + c - 'a';
-	if ('0' <= c && c <= '9') 
+	if ('0' <= c && c <= '9')
 		return 52 + c - '0';
-	if (c == '+') 
+	if (c == '+')
 		return 62;
-	if (c == '/') 
+	if (c == '/')
 		return 63;
 	return 64;
 }
@@ -195,13 +197,13 @@ struct base64_decoding_buffer* base64_create_decoding_buffer() {
  * 
  */
 void base64_free_decoding_buffer(struct base64_decoding_buffer *buf) {
-	
+
 	if( buf->s != NULL )
 		free(buf->s);
-	
+
 	if( buf->encoded != NULL )
 		free(buf->encoded);
-	
+
 	free(buf);
 }
 
@@ -269,27 +271,27 @@ void base64_decode_chunk(struct base64_decoding_buffer *buf, unsigned char *s, s
 		v = __base64_val(*p);
 
 		switch (have_bits) {
-		case 0: 
-			c = v; 
-			have_bits = 6; 
+		case 0:
+			c = v;
+			have_bits = 6;
 			break;
 
-		case 6: 
-			*o++ = (c << 2) | (v >> 4); 
-			c = v & 0x0F; 
-			have_bits = 4; 
+		case 6:
+			*o++ = (c << 2) | (v >> 4);
+			c = v & 0x0F;
+			have_bits = 4;
 			break;
 
-		case 4: 
-			*o++ = (c << 4) | (v >> 2); 
-			c = v & 0x03; 
-			have_bits = 2; 
+		case 4:
+			*o++ = (c << 4) | (v >> 2);
+			c = v & 0x03;
+			have_bits = 2;
 			break;
 
-		case 2: 
-			*o++ = (c << 6) | v; 
-			c = 0; 
-			have_bits = 0; 
+		case 2:
+			*o++ = (c << 6) | v;
+			c = 0;
+			have_bits = 0;
 			break;
 		}
 	}
